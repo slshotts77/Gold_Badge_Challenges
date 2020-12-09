1f1032f8b9c05a330d5a59ece71becd756871a97
Steve <slshotts77@gmail.com>
2020-12-09 17:05:46 -0500

    Gold_Badge_Challenge_One

---
+++ CafeTest/CafeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CafeTest
+{
+    [TestClass]
+    public class CafeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
