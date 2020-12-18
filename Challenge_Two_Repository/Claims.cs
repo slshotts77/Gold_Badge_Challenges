4e232e48d8c70511c5fbe1534bd14abb3ad25fad
Steve Shotts <73749169+slshotts77@users.noreply.github.com>
2020-12-18 04:53:22 -0500

    Add files via upload

---
+++ Challenge_Two_Repository/Claims.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Two_Repository
+{
+    public enum ClaimType
+    {
+        Car = 1, 
+        Home,
+        Theft
+    }
+
+    // Plan Old C# Object (POCO)
+    public class ClaimInfo
+    {                
+        /// </summary>
+        public int ClaimID { get; set; }
+        public ClaimType ClaimType { get; set; }
+        public string Description { get; set; }
+        public double ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+        public bool IsVaild { get; set; }
+
+        public ClaimInfo() { }
+
+        public ClaimInfo(int claimID, ClaimType claimType, string description, double claimAmount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+            IsValid = isValid;
+        }
+    }
+}
---
+++ Challenge_Two_Repository/ClaimsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Two_Repository
+{
+    public class ClaimsRepository
+    {
+        private Queue<ClaimInfo> _queueOfClaims = new Queue<ClaimInfo>();
+
+        // Create
+        public void AddNewClaim(ClaimInfo newClaim)
+        {
+            _queueOfClaims.Enqueue(newClaim);
+        }
+         
+        // Read
+        public Queue<ClaimInfo> GetAllClaims()
+        {
+                return _queueOfClaims;
+        }
+               
+        // Next Claim
+        public ClaimInfo NextClaim()
+        {
+            ClaimInfo nextClaim = _queueOfClaims.Peek();
+            Console.WriteLine("Here is the next claim:");
+            Console.WriteLine($"ClaimID: {nextClaim.ClaimID}\n");
+            Console.WriteLine($"ClaimType: {nextClaim.ClaimType}\n");
+            Console.WriteLine($"Description: {nextClaim.Description}\n");
+            Console.WriteLine($"ClaimAmount: {nextClaim.ClaimAmount}\n");
+            Console.WriteLine($"DateOfIncident: {nextClaim.DateOfIncident}\n");
+            Console.WriteLine($"DateOfClaim: {nextClaim.DateOfClaim}\n");
+            Console.WriteLine($"IsValid: {nextClaim.IsValid}");
+            return nextClaim;
+        }
+
+        //Remove Fist Queue
+        public void NextInQueue()
+        {
+            _queueOfClaims.Dequeue();        
+        }
+    }
+}
